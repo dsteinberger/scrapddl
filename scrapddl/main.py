f70f8e5ed2cab9b74b1a74e04d4da441b3fac0d6
david <david.steinberger@gmail.com>
2017-03-18 14:48:13 +0100

    Add class process

--- scrapddl/main.py
+++ scrapddl/main.py
@@ -3,13 +3,7 @@ from flask import render_template
 
 from flask_bootstrap import Bootstrap
 
-from spiders.extreme_down import EDMoviesSpider, EDTvShowsSpider
-from spiders.zone_telechargement import ZTMoviesSpider, ZTTvShowsSpider
-from spiders.ddl_island import DDLIMoviesSpider, DDLITvShowsSpider
-from spiders.golden_kai import GoldenKMangaSpider
-from spiders.univers_anime import UniversAnimeMangaSpider
-
-from items.items import GroupItem
+from process import Process
 
 
 def create_app():
@@ -23,53 +17,12 @@ app = create_app()
 
 @app.route("/")
 def home():
-    movies_group_items = GroupItem()
-    tvshows_group_items = GroupItem()
-    manga_group_items = GroupItem()
-
-    ed_movies_spider = EDMoviesSpider()
-    ed_movies_group_items = ed_movies_spider.parse()
-
-    ed_tvshows_spider = EDTvShowsSpider()
-    ed_tvshows_group_items = ed_tvshows_spider.parse()
-
-    zt_movies_spider = ZTMoviesSpider()
-    zt_movies_group_items = zt_movies_spider.parse()
-
-    zt_tvshows_spider = ZTTvShowsSpider()
-    zt_tvshows_group_items = zt_tvshows_spider.parse()
-
-    ddli_movies_spider = DDLIMoviesSpider()
-    ddli_movies_group_items = ddli_movies_spider.parse()
-
-    ddli_tvshows_spider = DDLITvShowsSpider()
-    ddli_tvshows_group_items = ddli_tvshows_spider.parse()
-
-    gk_manga_spider = GoldenKMangaSpider()
-    gk_manga_group_items = gk_manga_spider.parse()
-
-    ua_manga_spider = UniversAnimeMangaSpider()
-    ua_manga_group_items = ua_manga_spider.parse()
-
-    movies_group_items.zip_items([
-        ed_movies_group_items.items,
-        zt_movies_group_items.items,
-        ddli_movies_group_items.items])
-
-    tvshows_group_items.zip_items([
-        ed_tvshows_group_items.items,
-        zt_tvshows_group_items.items,
-        ddli_tvshows_group_items.items])
-
-    manga_group_items.zip_items([
-        gk_manga_group_items.items,
-        ua_manga_group_items.items
-    ])
-
+    process = Process()
+    process.process()
     return render_template('home.html',
-                           movies=movies_group_items.renderer(),
-                           tvshows=tvshows_group_items.renderer(),
-                           mangas=manga_group_items.renderer())
+                           movies=process.movies_group_items.renderer(),
+                           tvshows=process.tvshows_group_items.renderer(),
+                           mangas=process.manga_group_items.renderer())
 
 
 @app.route('/<path:path>')
---
+++ scrapddl/process.py
@@ -0,0 +1,62 @@
+from spiders.extreme_down import EDMoviesSpider, EDTvShowsSpider
+from spiders.zone_telechargement import ZTMoviesSpider, ZTTvShowsSpider
+from spiders.ddl_island import DDLIMoviesSpider, DDLITvShowsSpider
+from spiders.golden_kai import GoldenKMangaSpider
+from spiders.univers_anime import UniversAnimeMangaSpider
+
+from items.items import GroupItem
+
+
+class Process(object):
+
+    def __init__(self):
+        self.movies_group_items = GroupItem()
+        self.tvshows_group_items = GroupItem()
+        self.manga_group_items = GroupItem()
+
+    def process_movies(self):
+        ed_movies_spider = EDMoviesSpider()
+        ed_movies_group_items = ed_movies_spider.parse()
+
+        zt_movies_spider = ZTMoviesSpider()
+        zt_movies_group_items = zt_movies_spider.parse()
+
+        ddli_movies_spider = DDLIMoviesSpider()
+        ddli_movies_group_items = ddli_movies_spider.parse()
+
+        self.movies_group_items.zip_items([
+            ed_movies_group_items.items,
+            zt_movies_group_items.items,
+            ddli_movies_group_items.items])
+
+    def process_tvshows(self):
+        ed_tvshows_spider = EDTvShowsSpider()
+        ed_tvshows_group_items = ed_tvshows_spider.parse()
+
+        zt_tvshows_spider = ZTTvShowsSpider()
+        zt_tvshows_group_items = zt_tvshows_spider.parse()
+
+        ddli_tvshows_spider = DDLITvShowsSpider()
+        ddli_tvshows_group_items = ddli_tvshows_spider.parse()
+
+        self.tvshows_group_items.zip_items([
+            ed_tvshows_group_items.items,
+            zt_tvshows_group_items.items,
+            ddli_tvshows_group_items.items])
+
+    def process_mangas(self):
+        gk_manga_spider = GoldenKMangaSpider()
+        gk_manga_group_items = gk_manga_spider.parse()
+
+        ua_manga_spider = UniversAnimeMangaSpider()
+        ua_manga_group_items = ua_manga_spider.parse()
+
+        self.manga_group_items.zip_items([
+            gk_manga_group_items.items,
+            ua_manga_group_items.items
+        ])
+
+    def process(self):
+        self.process_movies()
+        self.process_tvshows()
+        self.process_mangas()
