8a2c7a55dcae98ef1f0e02814fd0cf08f4b42d0c
david <david.steinberger@gmail.com>
2017-06-24 15:28:31 +0200

    Add print source during process

--- scrapddl/process.py
+++ scrapddl/process.py
@@ -15,21 +15,27 @@ class Process(object):
         self.mangas_group_items = GroupItem()
 
     def process_movies(self):
+        print u"##  Extreme Download"
         ed_movies_spider = EDMoviesSpider()
         ed_movies_group_items = ed_movies_spider.parse()
 
+        print u"##  Extreme Download Hd"
         ed_movies_hd_spider = EDMoviesHDSpider()
         ed_movies_hd_group_items = ed_movies_hd_spider.parse()
 
+        print u"##  Zone Telechargement"
         zt_movies_spider = ZTMoviesSpider()
         zt_movies_group_items = zt_movies_spider.parse()
 
+        print u"##  Zone Telechargement Hd"
         zt_movies_hd_spider = ZTMoviesHDSpider()
         zt_movies_hd_group_items = zt_movies_hd_spider.parse()
 
+        print u"##  Ddl Island"
         ddli_movies_spider = DDLIMoviesSpider()
         ddli_movies_group_items = ddli_movies_spider.parse()
 
+        print u"##  Ddl Island Hd"
         ddli_movies_hd_spider = DDLIMoviesHDSpider()
         ddli_movies_hd_group_items = ddli_movies_hd_spider.parse()
 
@@ -42,12 +48,15 @@ class Process(object):
             ddli_movies_hd_group_items.items])
 
     def process_tvshows(self):
+        print u"##  Extreme Download"
         ed_tvshows_spider = EDTvShowsSpider()
         ed_tvshows_group_items = ed_tvshows_spider.parse()
 
+        print u"##  Zone Telechargement"
         zt_tvshows_spider = ZTTvShowsSpider()
         zt_tvshows_group_items = zt_tvshows_spider.parse()
 
+        print u"##  Ddl Island"
         ddli_tvshows_spider = DDLITvShowsSpider()
         ddli_tvshows_group_items = ddli_tvshows_spider.parse()
 
@@ -57,12 +66,15 @@ class Process(object):
             ddli_tvshows_group_items.items])
 
     def process_mangas(self):
+        print u"##  Golden Kai"
         gk_manga_spider = GoldenKMangaSpider()
         gk_mangas_group_items = gk_manga_spider.parse()
 
+        print u"##  Univers Anime"
         ua_manga_spider = UniversAnimeMangaSpider()
         ua_mangas_group_items = ua_manga_spider.parse()
 
+        print u"##  Zone Telechargement"
         zt_manga_spider = ZTMangaSpider()
         zt_mangas_group_items = zt_manga_spider.parse()
 
