31d5ea6d82d8a57bf2a3267a437d559ab2a39d56
dsteinberger <david.steinberger@gmail.com>
2025-12-06 07:21:24 +0100

    Refactor Process: extract common logic to _process_content method

--- scrapddl/process.py
+++ scrapddl/process.py
@@ -22,46 +22,29 @@ class Process(object):
         self.tvshows_group_items = GroupItem()
         self.mangas_group_items = GroupItem()
 
-    def process_movies(self):
+    def _process_content(self, content_type, spider_classes):
+        """Generic method to process any content type."""
         items_to_process = []
+        for spider_class in spider_classes:
+            if spider_class.is_activated():
+                print(f"## {content_type.upper()} {spider_class.name}")
+                spider = spider_class()
+                group_items = spider.parse()
+                items_to_process.append(group_items.items)
 
-        for movie_class in MOVIES_CLASS:
-            if movie_class.is_activated():
-                print(f"## MOVIE {movie_class.name}")
-                movies_spider = movie_class()
-                movies_group_items = movies_spider.parse()
-                items_to_process.append(movies_group_items.items)
+        getattr(self, f"{content_type}_group_items").zip_items(items_to_process)
 
-        self.movies_group_items.zip_items(items_to_process)
+    def process_movies(self):
+        self._process_content('movies', MOVIES_CLASS)
 
     def process_tvshows(self):
-        items_to_process = []
-
-        for tvshows_class in TVSHOWS_CLASS:
-            if tvshows_class.is_activated():
-                print(f"## TVSHOWS {tvshows_class.name}")
-                tvshows_spider = tvshows_class()
-                tvshows_group_items = tvshows_spider.parse()
-                items_to_process.append(tvshows_group_items.items)
-
-        self.tvshows_group_items.zip_items(items_to_process)
+        self._process_content('tvshows', TVSHOWS_CLASS)
 
     def process_mangas(self):
-        items_to_process = []
-
-        for mangas_class in MANGAS_CLASS:
-            if mangas_class.is_activated():
-                print(f"## MANGAS {mangas_class.name}")
-                mangas_spider = mangas_class()
-                mangas_group_items = mangas_spider.parse()
-                items_to_process.append(mangas_group_items.items)
-
-        self.mangas_group_items.zip_items(items_to_process)
+        self._process_content('mangas', MANGAS_CLASS)
 
     def has_process_object(self, section):
-        if len((getattr(self, f"{section}_group_items")).items) > 0:
-            return True
-        return False
+        return len(getattr(self, f"{section}_group_items").items) > 0
 
     def process(self):
         self.process_movies()
